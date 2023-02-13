9e837aed75bd4272a8c51606058244a1de34d602
Renata Deodório <renatadeod@gmail.com>
2023-02-13 20:04:59 -0300

    Métodos

--- cookbook.rb
+++ cookbook.rb
@@ -1,43 +1,55 @@
-puts 'Bem-vindo ao Cookbook, sua rede social de receitas'
+def bem_vindo()
+  puts 'Bem-vindo ao Cookbook, sua rede social de receitas'
+end
 
-receitas = []
-
-puts "[1] Cadastrar uma receita"
-puts "[2] Ver todas as receitas"
-puts "[3] Sair"
-
-print "Escolha uma opção: "
-opcao = gets.to_it()
-
-while(opcao != 3) do
-  if(opcao == 1)
-    puts "Digite o nome da receita: "
-    nome = gets.chomp()
-    puts "Digite o tipo da receita: "
-    tipo = gets.chomp()
-
-    receitas << { nome: nome, tipo: tipo }
-    puts
-    puts "Receita #{nome} cadastrada com sucesso!"
-    puts
-  elsif(opcao == 2)
-    puts "=========== Receitas cadastradas ==========="
-    receitas.each do |receita|
-      puts "#{receita[:nome]} - #{receita[:tipo]}"
-    end
-    puts
-  else
-    puts "Opção inválida"
-  end
-
+def menu ()
   puts "[1] Cadastrar uma receita"
   puts "[2] Ver todas as receitas"
   puts "[3] Sair"
 
   print "Escolha uma opção: "
-  opcao = gets.to_i()
+  return gets.to_i()
 end
 
-puts 'Obrigado por usar o Cookbook, até logo.
+def inserir_receita()
+  puts "Digite o nome da receita: "
+  nome = gets.chomp()
+  puts "Digite o tipo da receita: "
+  tipo = gets.chomp()
 
-'
+  puts
+  puts "Receita #{nome} cadastrada com sucesso!"
+  puts
+  return { nome: nome, tipo: tipo }
+end
+
+def imprimir_receitas(r)
+  puts "=========== Receitas cadastradas ==========="
+  r.each do |receita|
+    puts "#{receita[:nome]} - #{receita[:tipo]}"
+  end
+  puts
+end
+
+bem_vindo()
+
+receitas = []
+
+opcao = menu()
+
+while(opcao != 3) do
+  if(opcao == 1)
+    receitas << inserir_receita()
+  elsif(opcao == 2)
+    imprimir_receitas(receitas)
+  else  
+    puts "Opção Inválida"
+  end
+
+  opcao = menu()
+end
+
+
+puts 'Obrigado por usar o Cookbook, até logo.'
+
+
