0dea6929e03216f1ec4a94f727db1f68c1125825
Renata Deodório <renatadeod@gmail.com>
2023-02-13 17:07:36 -0300

    Iniciando Projeto

--- cookbook.rb
+++ cookbook.rb
@@ -1,6 +1,43 @@
 puts 'Bem-vindo ao Cookbook, sua rede social de receitas'
 
-puts 'Digite o nome da receita'
-gets
+receitas = []
 
-puts "Receita cadastrada com sucesso!"
+puts "[1] Cadastrar uma receita"
+puts "[2] Ver todas as receitas"
+puts "[3] Sair"
+
+print "Escolha uma opção: "
+opcao = gets.to_it()
+
+while(opcao != 3) do
+  if(opcao == 1)
+    puts "Digite o nome da receita: "
+    nome = gets.chomp()
+    puts "Digite o tipo da receita: "
+    tipo = gets.chomp()
+
+    receitas << { nome: nome, tipo: tipo }
+    puts
+    puts "Receita #{nome} cadastrada com sucesso!"
+    puts
+  elsif(opcao == 2)
+    puts "=========== Receitas cadastradas ==========="
+    receitas.each do |receita|
+      puts "#{receita[:nome]} - #{receita[:tipo]}"
+    end
+    puts
+  else
+    puts "Opção inválida"
+  end
+
+  puts "[1] Cadastrar uma receita"
+  puts "[2] Ver todas as receitas"
+  puts "[3] Sair"
+
+  print "Escolha uma opção: "
+  opcao = gets.to_i()
+end
+
+puts 'Obrigado por usar o Cookbook, até logo.
+
+'
